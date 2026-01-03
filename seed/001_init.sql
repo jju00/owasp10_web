a2874469cbd2f0a68aadc3cadbd5e41d5d956069
GHYoungKyun <dudrbsapdlf@naver.com>
2026-01-03 19:51:14 +0900

    SCRUM-61: 기타 자잘한 부분들 모두 수정

--- seed/001_init.sql
+++ seed/001_init.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(255) UNIQUE NOT NULL,
-  password CHAR(32) NOT NULL,   -- MD5 해시 길이는 32글자 (hex)
+  password CHAR(64) NOT NULL,   -- MD5 해시 길이는 32글자 (hex)
   role ENUM('admin','user') NOT NULL
 );
 
 -- 비밀번호를 MD5로 해싱해서 저장
 INSERT INTO users (username, password, role) VALUES
-  ('cookbob',   SHA2('NoBrute123'), 'admin'),
+  ('cookbob',   SHA1('NoBrute123'), 'admin'),
   ('nagox',   MD5('ashley76'), 'user'),
   ('alice',   MD5('alice123'),   'user'),
   ('bob',     MD5('bob123'),     'user'),
